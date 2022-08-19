1f614247c14b78ba38d708abccbd8d01eae3a907
Ali Osman Demirkollu <72684098+ali-osman-de@users.noreply.github.com>
2022-08-19 23:00:06 +0300

    Create reModule.py

---
+++ AdvancedPythonModules/reModule/reModule.py
@@ -0,0 +1,34 @@
+
+
+import re
+
+result = dir(re)
+
+# re module
+
+str = "Python Kursu: Python Programlama Rehberiniz!"
+
+# re.findall()
+
+# result = re.findall("Python", str)
+# print(len(result))
+
+# re.split()
+
+# result = re.split(" ",str)
+
+# re.sub()
+
+# result = re.sub(" ","-",str)
+
+# re.search()
+
+# result = re.search("Python",str)
+
+# result = result.span()
+# result = result.start()
+# result = result.end()
+# result = result.group()
+# result = result.string
+
+print(result)
